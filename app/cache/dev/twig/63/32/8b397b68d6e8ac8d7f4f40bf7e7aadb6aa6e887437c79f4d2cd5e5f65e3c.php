df55d1d88b6ea26bfc4fcfbb58902fdc1f78076c
yannL <lauret.yann@hotmail.fr>
2015-02-17 10:45:55 +0100

    Finish clearing dev cache

---
+++ app/cache/dev/twig/63/32/8b397b68d6e8ac8d7f4f40bf7e7aadb6aa6e887437c79f4d2cd5e5f65e3c.php
@@ -0,0 +1,57 @@
+<?php
+
+/* TwigBundle:Exception:traces.xml.twig */
+class __TwigTemplate_63328b397b68d6e8ac8d7f4f40bf7e7aadb6aa6e887437c79f4d2cd5e5f65e3c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "        <traces>
+";
+        // line 2
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable($this->getAttribute((isset($context["exception"]) ? $context["exception"] : $this->getContext($context, "exception")), "trace", array()));
+        foreach ($context['_seq'] as $context["_key"] => $context["trace"]) {
+            // line 3
+            echo "            <trace>
+";
+            // line 4
+            $this->env->loadTemplate("TwigBundle:Exception:trace.txt.twig")->display(array("trace" => $context["trace"]));
+            // line 5
+            echo "
+            </trace>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['trace'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 8
+        echo "        </traces>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:traces.xml.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  39 => 8,  31 => 5,  29 => 4,  26 => 3,  22 => 2,  19 => 1,);
+    }
+}
---
+++ app/cache/prod/twig/b6/bc/22f242a88db3e0fb87b29c4f7c6555d56e095a0f97b39b430e9d8a7c375b.php
@@ -0,0 +1,31 @@
+<?php
+
+/* TwigBundle:Exception:error.atom.twig */
+class __TwigTemplate_b6bc22f242a88db3e0fb87b29c4f7c6555d56e095a0f97b39b430e9d8a7c375b extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        $this->env->loadTemplate("TwigBundle:Exception:error.xml.twig")->display($context);
+    }
+
+    public function getTemplateName()
+    {
+        return "TwigBundle:Exception:error.atom.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
