9ed6bc4537e074b43a5185009bf4a82d8abe7fb1
yannL <lauret.yann@hotmail.fr>
2015-02-19 22:17:25 +0100

    Add data from database in saisie view

--- app/cache/dev/twig/45/48/83fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7bdecc.php
+++ app/cache/dev/twig/45/48/83fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7bdecc.php
@@ -14,6 +14,10 @@ class __TwigTemplate_454883fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7
             'stylesheets' => array($this, 'block_stylesheets'),
             'javascripts' => array($this, 'block_javascripts'),
             'menu' => array($this, 'block_menu'),
+            'date' => array($this, 'block_date'),
+            'historique' => array($this, 'block_historique'),
+            'nom' => array($this, 'block_nom'),
+            'deconnexion' => array($this, 'block_deconnexion'),
             'logo' => array($this, 'block_logo'),
             'fiche' => array($this, 'block_fiche'),
             'ligneforfait' => array($this, 'block_ligneforfait'),
@@ -50,27 +54,29 @@ class __TwigTemplate_454883fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7
       ";
         // line 19
         $this->displayBlock('menu', $context, $blocks);
-        // line 24
+        // line 43
         echo "
       ";
-        // line 25
+        // line 44
         $this->displayBlock('logo', $context, $blocks);
-        // line 30
+        // line 49
         echo "
+      <h1>Laboratoir Galaxy Swiss Bourdin</h1>
+      
       ";
-        // line 31
+        // line 52
         $this->displayBlock('fiche', $context, $blocks);
-        // line 96
+        // line 127
         echo "
       ";
-        // line 97
+        // line 128
         $this->displayBlock('saisieforfait', $context, $blocks);
-        // line 101
+        // line 132
         echo "
       ";
-        // line 102
+        // line 133
         $this->displayBlock('saisiehorsforfait', $context, $blocks);
-        // line 106
+        // line 137
         echo " 
     </div>
   </body>
@@ -107,46 +113,103 @@ class __TwigTemplate_454883fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7
     {
         // line 20
         echo "      <div id=\"menu\" class=\"container\">
-        liste du menu
-      </div>
+        ";
+        // line 21
+        $this->displayBlock('date', $context, $blocks);
+        // line 26
+        echo "        ";
+        $this->displayBlock('historique', $context, $blocks);
+        // line 31
+        echo "        ";
+        $this->displayBlock('nom', $context, $blocks);
+        // line 36
+        echo "        ";
+        $this->displayBlock('deconnexion', $context, $blocks);
+        // line 41
+        echo "      </div>
       ";
     }
 
-    // line 25
+    // line 21
+    public function block_date($context, array $blocks = array())
+    {
+        // line 22
+        echo "          <div id=\"dateMenu\">
+            date
+          </div>
+        ";
+    }
+
+    // line 26
+    public function block_historique($context, array $blocks = array())
+    {
+        // line 27
+        echo "          <div id=\"historiqueMenu\">
+            historique
+          </div>
+        ";
+    }
+
+    // line 31
+    public function block_nom($context, array $blocks = array())
+    {
+        // line 32
+        echo "          <div id=\"nomMenu\">
+            ";
+        // line 33
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["visiteur"]) ? $context["visiteur"] : $this->getContext($context, "visiteur")), "nom", array()), "html", null, true);
+        echo " ";
+        echo twig_escape_filter($this->env, $this->getAttribute((isset($context["visiteur"]) ? $context["visiteur"] : $this->getContext($context, "visiteur")), "prenom", array()), "html", null, true);
+        echo "
+          </div>
+        ";
+    }
+
+    // line 36
+    public function block_deconnexion($context, array $blocks = array())
+    {
+        // line 37
+        echo "          <div id=\"deconnexionMenu\">
+            deconnexion
+          </div>
+        ";
+    }
+
+    // line 44
     public function block_logo($context, array $blocks = array())
     {
-        // line 26
+        // line 45
         echo "      <div id=\"logo\" class=\"container\">
         <img   src=\"";
-        // line 27
+        // line 46
         echo twig_escape_filter($this->env, $this->env->getExtension('assets')->getAssetUrl("bundles/gsbapplifrais/images/logo.png"), "html", null, true);
         echo "\" class=\"imglogo\"/>
       </div>
       ";
     }
 
-    // line 31
+    // line 52
     public function block_fiche($context, array $blocks = array())
     {
-        // line 32
+        // line 53
         echo "        <div id=\"fiche\" class=\"container\">
           ";
-        // line 33
+        // line 54
         $this->displayBlock('ligneforfait', $context, $blocks);
-        // line 66
+        // line 93
         echo "
           ";
-        // line 67
-        $this->displayBlock('lignehorsforfait', $context, $blocks);
         // line 94
+        $this->displayBlock('lignehorsforfait', $context, $blocks);
+        // line 125
         echo "        </div>
       ";
     }
 
-    // line 33
+    // line 54
     public function block_ligneforfait($context, array $blocks = array())
     {
-        // line 34
+        // line 55
         echo "            <div id=\"ligneForfait\" class=\"container\">
               ligne forait
               <div id=\"libelleForfait\">
@@ -164,27 +227,55 @@ class __TwigTemplate_454883fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7
                 </div>
               </div>
               <div id=\"quantiteForfait\">
-                <div class=\"quantiteForfait\">
-                  0
-                </div>
-                <div class=\"quantiteForfait\">
-                  0
-                </div>
-                <div class=\"quantiteForfait\">
-                  0
-                </div>
-                <div class=\"quantiteForfait\">
-                  0
-                </div>
+
+                ";
+        // line 73
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable($this->getAttribute((isset($context["fiche"]) ? $context["fiche"] : $this->getContext($context, "fiche")), "forfaitLignes", array()));
+        foreach ($context['_seq'] as $context["_key"] => $context["ligneForfait"]) {
+            // line 74
+            echo "
+                  <div class=\"quantiteForfait\">
+                    ";
+            // line 76
+            echo twig_escape_filter($this->env, $this->getAttribute($context["ligneForfait"], "nbEtape", array()), "html", null, true);
+            echo "
+                  </div>
+                  <div class=\"quantiteForfait\">
+                    ";
+            // line 79
+            echo twig_escape_filter($this->env, $this->getAttribute($context["ligneForfait"], "nbKm", array()), "html", null, true);
+            echo "
+                  </div>
+                  <div class=\"quantiteForfait\">
+                    ";
+            // line 82
+            echo twig_escape_filter($this->env, $this->getAttribute($context["ligneForfait"], "nbNuit", array()), "html", null, true);
+            echo "
+                  </div>
+                  <div class=\"quantiteForfait\">
+                    ";
+            // line 85
+            echo twig_escape_filter($this->env, $this->getAttribute($context["ligneForfait"], "nbRepas", array()), "html", null, true);
+            echo "
+                  </div>
+
+                ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['ligneForfait'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 89
+        echo "
               </div>
             </div>
           ";
     }
 
-    // line 67
+    // line 94
     public function block_lignehorsforfait($context, array $blocks = array())
     {
-        // line 68
+        // line 95
         echo "            <div id=\"ligneHorsForfait\" class=\"container\">
               ligne hors forfait
               <div id=\"libelleHorsForfait\">
@@ -199,33 +290,56 @@ class __TwigTemplate_454883fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7
                 </div>
               </div>
               <div id=\"quantiteHorsForfait\">
-                <div class=\"quantiteHorsForfait\">
-                  0
-                </div>
-                <div class=\"descriptionHorsForfait\">
-                  0
-                </div>
-                <div class=\"quantiteHorsForfait\">
-                  0
-                </div>
+
+                ";
+        // line 110
+        $context['_parent'] = (array) $context;
+        $context['_seq'] = twig_ensure_traversable($this->getAttribute((isset($context["fiche"]) ? $context["fiche"] : $this->getContext($context, "fiche")), "horsForfaitLignes", array()));
+        foreach ($context['_seq'] as $context["_key"] => $context["ligneHorsForfait"]) {
+            // line 111
+            echo "                  <div class=\"quantiteHorsForfait\">
+                    ";
+            // line 112
+            echo twig_escape_filter($this->env, $this->getAttribute($this->getAttribute($context["ligneHorsForfait"], "date", array()), "format", array(0 => "d-m-Y"), "method"), "html", null, true);
+            echo "
+                  </div>
+                  <div class=\"descriptionHorsForfait\">
+                    ";
+            // line 115
+            echo twig_escape_filter($this->env, $this->getAttribute($context["ligneHorsForfait"], "libelle", array()), "html", null, true);
+            echo "
+                  </div>
+                  <div class=\"quantiteHorsForfait\">
+                    ";
+            // line 118
+            echo twig_escape_filter($this->env, $this->getAttribute($context["ligneHorsForfait"], "montant", array()), "html", null, true);
+            echo "€
+                  </div>
+                ";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['ligneHorsForfait'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+        // line 121
+        echo "
               </div>
             </div>  
           ";
     }
 
-    // line 97
+    // line 128
     public function block_saisieforfait($context, array $blocks = array())
     {
-        // line 98
+        // line 129
         echo "        <div id=\"saisieForfait\" class=\"container\">
         </div>
       ";
     }
 
-    // line 102
+    // line 133
     public function block_saisiehorsforfait($context, array $blocks = array())
     {
-        // line 103
+        // line 134
         echo "        <div id=\"saisieHorsForfait\" class=\"container\">
         </div>
       ";
@@ -238,6 +352,6 @@ class __TwigTemplate_454883fa58334fefdff405baf5e32e71c5f23c38b59b1826458e2b343f7
 
     public function getDebugInfo()
     {
-        return array (  229 => 103,  226 => 102,  220 => 98,  217 => 97,  188 => 68,  185 => 67,  150 => 34,  147 => 33,  142 => 94,  140 => 67,  137 => 66,  135 => 33,  132 => 32,  129 => 31,  122 => 27,  119 => 26,  116 => 25,  109 => 20,  106 => 19,  102 => 14,  99 => 13,  92 => 11,  89 => 10,  84 => 6,  81 => 5,  74 => 106,  72 => 102,  69 => 101,  67 => 97,  64 => 96,  62 => 31,  59 => 30,  57 => 25,  54 => 24,  52 => 19,  46 => 15,  43 => 13,  41 => 10,  37 => 8,  35 => 5,  29 => 1,);
+        return array (  343 => 134,  340 => 133,  334 => 129,  331 => 128,  324 => 121,  315 => 118,  309 => 115,  303 => 112,  300 => 111,  296 => 110,  279 => 95,  276 => 94,  269 => 89,  259 => 85,  253 => 82,  247 => 79,  241 => 76,  237 => 74,  233 => 73,  213 => 55,  210 => 54,  205 => 125,  203 => 94,  200 => 93,  198 => 54,  195 => 53,  192 => 52,  185 => 46,  182 => 45,  179 => 44,  172 => 37,  169 => 36,  160 => 33,  157 => 32,  154 => 31,  147 => 27,  144 => 26,  137 => 22,  134 => 21,  129 => 41,  126 => 36,  123 => 31,  120 => 26,  118 => 21,  115 => 20,  112 => 19,  108 => 14,  105 => 13,  98 => 11,  95 => 10,  90 => 6,  87 => 5,  80 => 137,  78 => 133,  75 => 132,  73 => 128,  70 => 127,  68 => 52,  63 => 49,  61 => 44,  58 => 43,  56 => 19,  50 => 15,  47 => 13,  45 => 10,  41 => 8,  39 => 5,  33 => 1,);
     }
 }
--- src/Gsb/AppliFraisBundle/Controller/visiteurController.php
+++ src/Gsb/AppliFraisBundle/Controller/visiteurController.php
@@ -4,12 +4,34 @@ namespace Gsb\AppliFraisBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use \DateTime;
+
 class VisiteurController extends Controller
 {
     public function saisieAction($id)
-    {
+    {   
+        $ficheEnCours = false;
+
+        $em = $this->getDoctrine()->getManager();
+
+        $visiteur = $em->getRepository('GsbAppliFraisBundle:Employe')->find($id);
+        $fiches = $visiteur->getFiches();
+        $derniereFiche = $fiches[sizeof($fiches)-1];
+        $date = new DateTime();
+
+        if ($derniereFiche->getDate()->format('Y-m') != $date->format('Y-m')){
+            if ($derniereFiche->getEtat()->getLibelle() != 'Cloturée'){
+
+                $etatClot = $em->getRepository('GsbAppliFraisBundle:Employe')->findByLibelle('Cloturée');
+
+                $derniereFiche->setEtat($etatClot);
+            }
+        }
+
+        
         return $this->render('GsbAppliFraisBundle:Visiteur:saisie.html.twig', array(
-                'visiteur' => $id,
+                'visiteur' => $visiteur,
+                'fiche' => $derniereFiche,
             ));
     }
     
